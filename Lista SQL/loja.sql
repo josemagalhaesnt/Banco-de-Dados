8b44c9b75d3dfd98273e57550d834b4cda025bc7
Zéh Neto <zneto@live.com>
2017-12-11 18:35:17 -0300

    Adicionados valores às tabelas

--- Lista SQL/loja.sql
+++ Lista SQL/loja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 11-Dez-2017 às 21:31
+-- Generation Time: 11-Dez-2017 às 22:34
 -- Versão do servidor: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -37,6 +37,15 @@ CREATE TABLE `clientes` (
   `telefone` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nome`, `data_nasc`, `endereco`, `cpf`, `telefone`) VALUES
+(1, 'Xico Lucas', '0000-00-00', 'Rua das Putarias, 69', '444.444.444-44', '(85)1313-1551'),
+(2, 'Luana Albuquerque', '0000-00-00', 'Rua das Putarias, 24', '454.454.444-44', '(85)1243-1551'),
+(7, 'Xuliana Cardoso', '0000-00-00', 'Rua das Putarias, 11', '324.454.444-44', '(85)1223-2469');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +56,16 @@ CREATE TABLE `itens_da_venda` (
   `id_venda` int(11) NOT NULL,
   `id_produto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `itens_da_venda`
+--
+
+INSERT INTO `itens_da_venda` (`id_venda`, `id_produto`) VALUES
+(1, 1),
+(2, 2),
+(4, 1),
+(5, 3);
 
 -- --------------------------------------------------------
 
@@ -63,6 +82,17 @@ CREATE TABLE `produtos` (
   `qt_estoque` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome_produto`, `descricao`, `preco_compra`, `preco_venda`, `qt_estoque`) VALUES
+(1, 'Pênis de borracha 28cm', 'Consolo com vibrador com 5 níveis de vibração', 100, 159.9, NULL),
+(2, 'Pênis de borracha 25cm', 'Consolo com vibrador com 5 níveis de vibração', 80, 119.9, NULL),
+(3, 'Pênis de borracha 22cm', 'Consolo com vibrador com 3 níveis de vibração', 50, 99.9, NULL),
+(4, 'Pênis de borracha 17cm', 'Consolo com vibrador com 3 níveis de vibração', 30, 79.9, NULL),
+(5, 'Pênis de borracha 15cm', 'Consolo com vibrador com 3 níveis de vibração', 25, 59.9, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -73,9 +103,19 @@ CREATE TABLE `vendas` (
   `id_venda` int(11) NOT NULL,
   `cliente` int(11) NOT NULL,
   `vendedor` int(11) NOT NULL,
-  `valor_total` float NOT NULL,
-  `metodo_pag` tinyint(4) NOT NULL
+  `metodo_pag` tinyint(4) NOT NULL,
+  `descricao_pag` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `vendas`
+--
+
+INSERT INTO `vendas` (`id_venda`, `cliente`, `vendedor`, `metodo_pag`, `descricao_pag`) VALUES
+(1, 1, 2, 1, 'Boquecard'),
+(2, 2, 1, 2, 'Mastercu'),
+(4, 2, 1, 2, 'Mastercu'),
+(5, 2, 1, 3, 'Dinhayronn');
 
 -- --------------------------------------------------------
 
@@ -90,6 +130,16 @@ CREATE TABLE `vendedores` (
   `cargo` char(1) NOT NULL,
   `descricao_cargo` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `vendedores`
+--
+
+INSERT INTO `vendedores` (`id_vendedor`, `nome`, `cpf`, `cargo`, `descricao_cargo`) VALUES
+(1, 'Alcides Junior', '000.000.000-00', 'v', 'Vendedor'),
+(2, 'Anderson Alencar', '111.111.111-11', 'v', 'Vendedor'),
+(3, 'José Neto', '222.222.222-22', 'g', 'Gerente'),
+(4, 'Cintia Cassia', '333.333.333-33', 'c', 'Caixa');
 
 --
 -- Indexes for dumped tables
@@ -141,25 +191,25 @@ ALTER TABLE `vendedores`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `vendas`
 --
 ALTER TABLE `vendas`
-  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `vendedores`
 --
 ALTER TABLE `vendedores`
-  MODIFY `id_vendedor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_vendedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
